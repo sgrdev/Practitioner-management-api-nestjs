06a89c01f32a87987907bea915b40bdfdb332216
sgr2a <95933073+sgrdev@users.noreply.github.com>
2024-03-11 21:20:07 +0545

    made-code-more-secure-using-env

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient{
         super({
             datasources:{
                 db:{
-                   url:"postgresql://postgres:123@localhost:5434/nest?schema=public" 
+                   url: process.env.DATABASE_URL
                 }
             }
         })
